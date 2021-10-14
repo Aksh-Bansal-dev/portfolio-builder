0bd32639d7ac05cb9415ce7e5f0744699fde985e
Aksh Bansal <akshmark@gmail.com>
2021-10-14 13:24:02 +0530

    fix(client): undefined variables

--- client/src/components/FormikTextField.tsx
+++ client/src/components/FormikTextField.tsx
@@ -1,7 +1,6 @@
 import { TextField } from "@material-ui/core";
 import { FieldHookConfig, useField } from "formik";
 import React from "react";
-import { useSignupLoginStyles } from "./Forms/signupLoginStyle";
 
 type FormikTextFieldProps = {
   className: string;
@@ -17,7 +16,6 @@ const FormikTextField: React.FC<FormikTextFieldProps> = ({
   ...props
 }) => {
   // eslint-disable-next-line @typescript-eslint/ban-types
-  const classes = useSignupLoginStyles();
   const [field, meta] = useField<string>(props);
   const errorText = meta.error && meta.touched ? meta.error : "";
   const obj = {
@@ -31,19 +29,6 @@ const FormikTextField: React.FC<FormikTextFieldProps> = ({
       {...field}
       helperText={errorText}
       error={!!errorText}
-      InputLabelProps={{
-        classes: {
-          root: classes.cssLabel,
-          focused: classes.cssFocused,
-        },
-      }}
-      InputProps={{
-        classes: {
-          root: classes.cssOutlinedInput,
-          focused: classes.cssFocused,
-          notchedOutline: classes.notchedOutline,
-        },
-      }}
       variant="outlined"
     />
   );
